7fc484c721fe8ea912aac1efe1dccb53f90cfd9e
vicentecrespop <vicentecrespo45@gmail.com>
2022-12-07 14:37:44 -0300

    atualizei db

--- src/config/config.php
+++ src/config/config.php
@@ -1,15 +1,16 @@
 <?php
 
-    $dbname = "db_teste_dev";
-    $server = "db-teste-dev.c1ba0xprdutv.us-east-1.rds.amazonaws.com";
-    $user = "vicentecrespop";
-    $password = "vicente123";
+    $dbname = "d4ntsjcj95tffr";
+    $server = "ec2-54-163-34-107.compute-1.amazonaws.com";
+    $user = "ldcgkpgorylhao";
+    $password = "c5e3039ed75c33c586faf9b0ef0a8c54a38983ab142a4770f716fe525c7df4f5";
     $port = "5432";
     $driver = "pgsql";
+    $uri = 'postgres://ldcgkpgorylhao:c5e3039ed75c33c586faf9b0ef0a8c54a38983ab142a4770f716fe525c7df4f5@ec2-54-163-34-107.compute-1.amazonaws.com:5432/d4ntsjcj95tffr';
     $connect;
 
-    $pdo = new PDO("{$driver}:host={$server};port={$port};dbname={$dbname}", $user, $password);
-    
+    $pdo = new PDO($uri, $user, $password);
+
 ?>
 
 
